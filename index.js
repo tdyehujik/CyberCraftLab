71ad0b97c4531603cde00522c4af44138213d551
tdyehujik <gs1kenz.pou@hotmail.com>
2024-05-11 10:50:20 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-function isValidSudoku(board) {
-  for (let i = 0; i < 9; i++) {
-    const row = new Set();
-    const col = new Set();
-    const box = new Set();
-    for (let j = 0; j < 9; j++) {
-      const rowVal = board[i][j];
-      const colVal = board[j][i];
-      const boxVal =
-        board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
-      if (rowVal !== ".") {
-        if (row.has(rowVal)) return false;
-        row.add(rowVal);
-      }
-      if (colVal !== ".") {
-        if (col.has(colVal)) return false;
-        col.add(colVal);
-      }
-      if (boxVal !== ".") {
-        if (box.has(boxVal)) return false;
-        box.add(boxVal);
-      }
+function partitionLabels(s) {
+  const last = new Array(26);
+  for (let i = 0; i < s.length; i++) {
+    last[s.charCodeAt(i) - "a".charCodeAt(0)] = i;
+  }
+  let anchor = 0;
+  let j = 0;
+  const result = [];
+  for (let i = 0; i < s.length; i++) {
+    j = Math.max(j, last[s.charCodeAt(i) - "a".charCodeAt(0)]);
+    if (i === j) {
+      result.push(i - anchor + 1);
+      anchor = i + 1;
     }
   }
-  return true;
+  return result;
 }
